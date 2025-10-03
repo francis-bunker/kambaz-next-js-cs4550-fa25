ef9df774107cde8a92cbdf1f6cc21d0b9ed561de
francis-bunker <185110850+francis-bunker@users.noreply.github.com>
2025-10-03 19:15:38 -0400

    hw2

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
                 <NavLink href="/" as={Link}>Kambaz</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink href="https://github.com/jannunzi">My GitHub</NavLink>
+                <NavLink href="https://github.com/francis-bunker/kambaz-next-js-cs4550-fa25">My GitHub</NavLink>
             </NavItem>
         </Nav>
     );
