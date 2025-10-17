2247ccd3cc77a7dffe3a917627d2f42c64133af2
francis-bunker <185110850+francis-bunker@users.noreply.github.com>
2025-10-17 19:41:51 -0400

    a3 JavaScript

--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useParams } from "next/navigation";
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
