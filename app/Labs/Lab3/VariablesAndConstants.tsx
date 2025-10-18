f335a77d014315bfade69137a4950f45d5ca53e6
francis-bunker <185110850+francis-bunker@users.noreply.github.com>
2025-10-17 20:02:14 -0400

    a3

--- app/Labs/Lab3/VariablesAndConstants.tsx
+++ app/Labs/Lab3/VariablesAndConstants.tsx
@@ -5,8 +5,8 @@ export default function VariablesAndConstants() {
     return (
         <div id="wd-variables-and-constants">
             <h4>Variables and Constants</h4>
-            functionScoped = {functionScoped}<br />
-            blockScoped = {blockScoped}<br />
+            let functionScoped = {functionScoped}<br />
+            let blockScoped = {blockScoped}<br />
             constant1 = {constant1}<hr />
         </div>
     );
