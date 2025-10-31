4a2f7bf9b2a1afd24b2536348ed3b0166c6a6e5e
francis-bunker <185110850+francis-bunker@users.noreply.github.com>
2025-10-31 15:11:05 -0400

    bug fix

--- app/Labs/Lab4/ReduxExamples/CounterRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/CounterRedux/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./counterReducer";
 /* eslint-disable @typescript-eslint/no-explicit-any */
