41746a51fefc83ba6da2d1cfdb464ade323db86d
francis-bunker <185110850+francis-bunker@users.noreply.github.com>
2025-10-31 15:12:51 -0400

    bug fix

--- app/Labs/Lab4/ReduxExamples/HelloRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/HelloRedux/page.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 import { useSelector, useDispatch } from "react-redux";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default function HelloRedux() {
