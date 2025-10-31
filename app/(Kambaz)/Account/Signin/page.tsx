944e0c3eec09fa3136988f365698da163929a196
francis-bunker <185110850+francis-bunker@users.noreply.github.com>
2025-10-31 14:58:38 -0400

    bug fix

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Link from "next/link";
 import { redirect } from "next/dist/client/components/navigation";
 import { setCurrentUser } from "../reducer";
